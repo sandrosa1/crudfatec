3c5c33ed00e7c4965931fda570b607de0c78ae2b
sandrosa1 <sandrosa@fatec.sp.gov.br>
2021-04-14 01:05:11 -0300

    finalizado12

--- services/conexao.php
+++ services/conexao.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $connection = new PDO("mysql:host=localhost;port=3306;dbname=teste",'root','Jpthlusa15*m');
+    $connection = new PDO("mysql:host=localhost;port=3306;dbname=teste",'root','');
 } catch (Exception $error) {
     echo "Ocorreu o seguite erro: {$error->getMessage()}";
     
